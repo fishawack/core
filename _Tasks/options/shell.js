dd28f74fc4ec1a9055f373dc82543ad4b457fa4b
Mike Mellor <mike.mellor@f-grp.com>
2018-05-31 14:46:12 +0100

    create dirs on curl

--- _Tasks/options/shell.js
+++ _Tasks/options/shell.js
@@ -13,7 +13,7 @@ module.exports = {
         command: "curl -X PUT -F file=@_Zips/Deploy.zip -F 'data={\"keys\":{\"ios\":{\"id\":<%= (contentJson.attributes.phonegap && contentJson.attributes.phonegap.signingKey || '') %>,\"password\":\"13Orange02\"}}}' https://build.phonegap.com/api/v1/apps/<%= (contentJson.attributes.phonegap && contentJson.attributes.phonegap.appID || '') %>?auth_token=9tsERPEn2PamsbzFFjAB"
     },
     pullApp: {
-        command: "curl -o _Packages/iOS/app.ipa <%= pullApp %>"
+        command: "curl --create-dirs -o _Packages/iOS/app.ipa <%= pullApp %>"
     },
     pdf: {
         command: "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile='_Pdfs/<%= contentJson.attributes.title %>_<%= pkg.version %>_<%= grunt.template.today('dd-mm-yy') %>.pdf' '_Pdfs/raw.pdf'"
