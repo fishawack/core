a2e3de85a15e4147d1a89d6853e15844bd84bc4e
Mike Mellor <mike.mellor@f-grp.com>
2018-06-11 15:02:26 +0100

    ratio pdf ability

--- _Node/crawl.js
+++ _Node/crawl.js
@@ -14,21 +14,35 @@ var captureIndex = 0;
 grunt.file.mkdir('.tmp/screenshots/');
 
 var qualitySizes = {
-	hd: [1920, 1080],
-	hd_retina: [960, 540],
-	sd: [1080, 608],
-	test: [16, 9]
+    '16:9': {
+    	hd: [1920, 1080],
+    	hd_retina: [960, 540],
+    	sd: [1080, 608],
+    	test: [16, 9]
+    },
+    '4:3': {
+        hd: [1366, 1024],
+        hd_retina: [683, 512],
+        sd: [1024, 768],
+        test: [20, 15]
+    }
 };
+
+var ratio = '16:9';
 
 var quality = 'sd';
 
 if(typeof deployEnv.pdf === 'string'){
     quality = deployEnv.pdf;
+
+    if(deployEnv.ratio){
+        ratio = deployEnv.ratio;
+    }
 }
 
 browser.setViewportSize({
-    width: qualitySizes[quality][0],
-    height: qualitySizes[quality][1]
+    width: qualitySizes[ratio][quality][0],
+    height: qualitySizes[ratio][quality][1]
 });
 
 if(typeof capturePage === 'function'){
