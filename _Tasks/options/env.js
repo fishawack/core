ba65e6f7532230a87e840d6519e521bbdcc973e6
Mike Mellor <mike.mellor@f-grp.com>
2018-09-27 10:50:36 +0100

    can now have env in contentJSon and overwrite in deploy targets

--- _Tasks/options/env.js
+++ _Tasks/options/env.js
@@ -1,12 +1,17 @@
 module.exports = {
+	options: {
+		add : contentJson.attributes.env
+	},
 	dev : {
 		NODE_ENV : 'development',
 		NODE_IP : '<%= getIP() %>'
     },
-    dist : {
-		NODE_ENV : 'production'
-    },
     stage : {
-		NODE_ENV : 'staging'
+		NODE_ENV : 'staging',
+		concat : contentJson.attributes.staging && contentJson.attributes.staging.env || {}
+    },
+    dist : {
+		NODE_ENV : 'production',
+		concat : contentJson.attributes.production && contentJson.attributes.production.env || {}
     }
 }
