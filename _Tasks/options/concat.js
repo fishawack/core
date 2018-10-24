090ab369a18e791ff14d42ae9241f9b0ed506bf4
Mike Mellor <mike.mellor@f-grp.com>
2018-10-24 15:29:55 +0100

    ++ files will now bundle js without browserify

--- _Tasks/options/concat.js
+++ _Tasks/options/concat.js
@@ -6,12 +6,32 @@ module.exports = {
         files: [
         {
             '<%= root %>/js/crucial.js': ['_Build/js/**/__*.js']
+        },
+        {
+            expand: true,
+            cwd: '_Build/js/',
+            src: ['**/++*.js'],
+            dest: '<%= root %>/js/',
+            flatten: true,
+            rename: function(path, file){
+                return path + file.replace('++', '');
+            }
         }]
     },
     dist: {
         files: [
         {
             '.tmp/js/crucial.js': ['_Build/js/**/__*.js']
+        },
+        {
+            expand: true,
+            cwd: '_Build/js/',
+            src: ['**/++*.js'],
+            dest: '.tmp/js/',
+            flatten: true,
+            rename: function(path, file){
+                return path + file.replace('++', '');
+            }
         }]
     }
 }
