1375e94b7546c1f325c6afabdcb1c39ff74def4a
Mike Mellor <mike.mellor@f-grp.com>
2019-10-14 14:58:28 +0100

    updated gitlog to now work again / removed gitlog stand alone task

--- _Tasks/mail.js
+++ _Tasks/mail.js
@@ -32,44 +32,67 @@ module.exports = function(grunt) {
             recipients.push('digital@f-grp.com');
         }
 
-        nodemailer['deploy'] = {
-            options: {
-                recipients: recipients,
-                message: {
-                    from: "fishawack.auto.package@gmail.com",
-                    subject: 'Auto-package: - <%= contentJson.attributes.title %>',
-                    html: String.format(buildHtmlEmail('base'), 
-                        [
-                            buildHtmlEmail('target'),
-                            buildHtmlEmail('date'),
-                            (contentJson.attributes.code) ? buildHtmlEmail('code') : '',
-                            (contentJson.attributes.design) ? buildHtmlEmail('design') : '',
-                            (contentJson.attributes.cms) ? buildHtmlEmail('cms') : '',
-                            buildHtmlEmail('url'),
-                            (deployEnv.users) ? buildHtmlEmail('users') : '',
-                            buildHtmlEmail('issues')
-                        ].join(''),
-                        [
-                            (deployEnv.pdf) ? buildHtmlEmail('pdf') : '',
-                            (deployTarget === 'production') ? buildHtmlEmail('zips') : ''
-                        ].join(''),
-                        [
-                            buildHtmlEmail('version'),
-                            buildHtmlEmail('coverage'),
-                            (deployEnv.pdf) ? buildHtmlEmail('diff') : '',
-                            buildHtmlEmail('status'),
-                            (deployTarget === 'production' && contentJson.attributes.phonegap) ? buildHtmlEmail('phonegap') : '',,
-                            buildHtmlEmail('instance'),
-                            (contentJson.attributes.googleTrackingID) ? buildHtmlEmail('google') : '',
-                            buildHtmlEmail('git')
-                        ].join('')
-                    )
+        var colors = ['red', 'blue', 'green', 'purple', '#FF5516'];
+        var done = this.async();
+
+        require('gitlog')({ 
+                repo: __dirname,
+                number: 5,
+                fields: [ 'hash', 'subject', 'abbrevHash', 'committerName', 'committerDateRel']
+            }, 
+            function(error, commits) {
+                for(var i = 0, len = commits.length; i < len; i++){
+                    gitLogString += '<li style="color: ' + colors[i % colors.length] + ';"><b>Commit: </b>' + commits[i].abbrevHash + '<ul style="color: black;">';
+
+                    gitLogString += '<li><strong>Name</strong>: ' + commits[i].committerName + '</li>';
+                    gitLogString += '<li><strong>Date</strong>: ' + commits[i].committerDateRel + '</li>';
+                    gitLogString += '<li><strong>Message</strong>: ' + commits[i].subject + '</li>';
+
+                    gitLogString += '</ul></li>';
                 }
+
+                nodemailer['deploy'] = {
+                    options: {
+                        recipients: recipients,
+                        message: {
+                            from: "fishawack.auto.package@gmail.com",
+                            subject: 'Auto-package: - <%= contentJson.attributes.title %>',
+                            html: String.format(buildHtmlEmail('base'), 
+                                [
+                                    buildHtmlEmail('target'),
+                                    buildHtmlEmail('date'),
+                                    (contentJson.attributes.code) ? buildHtmlEmail('code') : '',
+                                    (contentJson.attributes.design) ? buildHtmlEmail('design') : '',
+                                    (contentJson.attributes.cms) ? buildHtmlEmail('cms') : '',
+                                    buildHtmlEmail('url'),
+                                    (deployEnv.users) ? buildHtmlEmail('users') : '',
+                                    buildHtmlEmail('issues')
+                                ].join(''),
+                                [
+                                    (deployEnv.pdf) ? buildHtmlEmail('pdf') : '',
+                                    (deployTarget === 'production') ? buildHtmlEmail('zips') : ''
+                                ].join(''),
+                                [
+                                    buildHtmlEmail('version'),
+                                    buildHtmlEmail('coverage'),
+                                    (deployEnv.pdf) ? buildHtmlEmail('diff') : '',
+                                    buildHtmlEmail('status'),
+                                    (deployTarget === 'production' && contentJson.attributes.phonegap) ? buildHtmlEmail('phonegap') : '',,
+                                    buildHtmlEmail('instance'),
+                                    (contentJson.attributes.googleTrackingID) ? buildHtmlEmail('google') : '',
+                                    buildHtmlEmail('git')
+                                ].join('')
+                            )
+                        }
+                    }
+                };
+
+                grunt.config.set('nodemailer', nodemailer);
+
+                grunt.task.run('nodemailer');
+
+                done();
             }
-        };
-
-        grunt.config.set('nodemailer', nodemailer);
-
-        grunt.task.run('gitLog', 'nodemailer');
+        );
     });
 };
