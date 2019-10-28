07a410e24e11418ef4d4d556a21ff633d28fc657
Mike Mellor <mike.mellor@f-grp.com>
2019-10-28 18:26:31 +0000

    badges now moved to validate

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,11 +51,11 @@ module.exports = function(grunt) {
     // Setup custom template handlebar code
     templateCustom();        
 
-    grunt.registerTask('default', ['clean:cache', 'env:dev', 'karma:unit:start', 'badges', 'jshint', 'modernizr', 'tv4', 'webpack:dev', 'concat:dev', 'fontello_svg', 'svgfit', 'svgmin', 'svg_sprite', 'copy:content', 'copy:assets', 'copy:svg', 'copy:svgasis', 'compile-handlebars', 'htmlmin', 'compile-vue', 'sass', 'postcss:dev', 'clean:build', 'browserSync', 'watch']);
+    grunt.registerTask('default', ['clean:cache', 'env:dev', 'karma:unit:start', 'jshint', 'modernizr', 'tv4', 'webpack:dev', 'concat:dev', 'fontello_svg', 'svgfit', 'svgmin', 'svg_sprite', 'copy:content', 'copy:assets', 'copy:svg', 'copy:svgasis', 'compile-handlebars', 'htmlmin', 'compile-vue', 'sass', 'postcss:dev', 'clean:build', 'browserSync', 'watch']);
     
-    grunt.registerTask('dist', ['env:dist', 'clean:dist', 'badges', 'jshint', 'modernizr', 'tv4', 'webpack:dist', 'concat:dist', 'uglify:dist', 'fontello_svg', 'svgfit', 'svgmin', 'svg_sprite', 'copy:content', 'copy:assets', 'copy:svg', 'copy:svgasis', 'compile-handlebars', 'htmlmin', 'compile-vue', 'sass', 'postcss:dist', 'cacheBust', 'imagemin', 'clean:build']);
+    grunt.registerTask('dist', ['env:dist', 'clean:dist', 'jshint', 'modernizr', 'tv4', 'webpack:dist', 'concat:dist', 'uglify:dist', 'fontello_svg', 'svgfit', 'svgmin', 'svg_sprite', 'copy:content', 'copy:assets', 'copy:svg', 'copy:svgasis', 'compile-handlebars', 'htmlmin', 'compile-vue', 'sass', 'postcss:dist', 'cacheBust', 'imagemin', 'clean:build']);
 
-    grunt.registerTask('validate', ['jshint', 'tv4', 'connect', 'casperjs:local', 'karma:continuous', 'coverage']);
+    grunt.registerTask('validate', ['jshint', 'tv4', 'connect', 'casperjs:local', 'karma:continuous', 'badges', 'coverage']);
 };
 
 function loadConfig(path) {
