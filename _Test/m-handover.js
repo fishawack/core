d0f64bef2b374366fc80f5553da1f583e64639c7
Mike Mellor <mike.mellor@f-grp.com>
2020-09-17 12:44:46 +0100

    added handover test

---
+++ _Test/m-handover.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fs = require('fs');
+const expect = require('chai').expect;
+const grunt = require('grunt');
+const execSync = require('child_process').execSync;
+const path = require('path');
+const glob = require('glob');
+
+describe('handover', () => {
+    before(() => {
+        execSync('grunt cegedim --branch=master --mocha=bundle', {encoding: 'utf8', stdio: 'pipe'});
+    });
+    
+    it('Should pull in the build files', () => {
+        expect(glob.sync(path.join(__dirname, '_fixture/bundle/_Packages/Handover/_Build/**/*'))).to.have.length;
+    });
+});
