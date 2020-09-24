8dc3e550ca6a1e509af806d7a1299c0866a6ec73
Mike Mellor <mike.mellor@f-grp.com>
2020-09-24 14:12:20 +0100

    refactored tests

--- _Test/deploy.js
+++ _Test/deploy.js
@@ -4,34 +4,14 @@ const expect = require('chai').expect;
 const execSync = require('child_process').execSync;
 const fetch = require('node-fetch');
 
+async function deploy(branch){
+    expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.not.equal(200);
+    execSync(`grunt package deploy --branch=${branch} --mocha=output`, {encoding: 'utf8', stdio: 'pipe'});
+    expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.be.equal(200);
+    execSync(`grunt takedown --branch=${branch} --mocha=output`, {encoding: 'utf8', stdio: 'pipe'});
+}
+
 describe('deploy', () => {
-    var branch = '';
-
-    beforeEach(() => {
-    });
-
-    it('Should deploy the master target to the server via sftp', async () => {
-        branch = 'master';
-        expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.not.equal(200);
-        execSync(`grunt package deploy --branch=${branch} --mocha=output`, {encoding: 'utf8', stdio: 'pipe'});
-        expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.be.equal(200);
-    });
-
-    it('Should deploy a watertight wrapped site to the server', async () => {
-        branch = 'watertight';
-        expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.not.equal(200);
-        execSync(`grunt package deploy --branch=${branch} --mocha=output`, {encoding: 'utf8', stdio: 'pipe'});
-        expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy/login')).status).to.be.equal(200);
-    });
-
-    // it('Should deploy the lftp target to the server via lftp', async () => {
-    //     branch = 'lftp';
-    //     expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.not.equal(200);
-    //     execSync(`grunt package deploy --branch=${branch} --mocha=output`, {encoding: 'utf8', stdio: 'pipe'});
-    //     expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.be.equal(200);
-    // });
-
-    afterEach(() => {
-        execSync(`grunt takedown --branch=${branch} --mocha=output`, {encoding: 'utf8', stdio: 'pipe'}); 
-    });
+    it('Should deploy the master target to the server via sftp', () => deploy('master'));
+    it('Should deploy a watertight wrapped site to the server', () => deploy('watertight'));
 });
