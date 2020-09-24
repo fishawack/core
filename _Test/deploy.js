4bfd6832ada26c3797fe1bb6b310bc033b555fb8
Mike Mellor <mike.mellor@f-grp.com>
2020-09-24 22:05:12 +0100

    regen package-lock / rupdated deploy test

--- _Test/deploy.js
+++ _Test/deploy.js
@@ -5,6 +5,7 @@ const execSync = require('child_process').execSync;
 const fetch = require('node-fetch');
 
 async function deploy(branch){
+    execSync(`grunt takedown --branch=${branch} --mocha=output`, {encoding: 'utf8', stdio: 'pipe'});
     expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.not.equal(200);
     execSync(`grunt package deploy --branch=${branch} --mocha=output`, {encoding: 'utf8', stdio: 'pipe'});
     expect((await fetch('https://demo.fishawack.solutions/core-test-suite-deploy')).status).to.be.equal(200);
