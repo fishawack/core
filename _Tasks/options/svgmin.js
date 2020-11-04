dbe1143a17e1d790a1df72a2ba085b2f36955600
Mike Mellor <mike.mellor@f-grp.com>
2020-11-04 18:15:28 +0000

    refactored svgmin option as a lot of redundant code

--- _Tasks/options/svgmin.js
+++ _Tasks/options/svgmin.js
@@ -1,74 +1,40 @@
+var plugins = [
+    { removeViewBox: false, },
+    { removeDimensions: true, },
+    {
+        cleanupIDs: {
+            prefix: {
+                toString() {
+                    this.counter = this.counter || 0;
+
+                    return `id-${this.counter++}`;
+                }
+            }
+        }
+    }
+];
+
 module.exports = {
-    stripAll: {
+    full: {
         options: {
-            plugins: [
-                { removeViewBox: false, },
-                { removeDimensions: true, },
+            plugins: plugins.concat(
                 { removeStyleElement: true },
                 { removeUselessStrokeAndFill: true },
-                { removeAttrs: {attrs: '(stroke|fill)'} },
-                {
-                    cleanupIDs: {
-                        prefix: {
-                            toString() {
-                                this.counter = this.counter || 0;
-
-                                return `id-${this.counter++}`;
-                            }
-                        }
-                    }
-                }
-            ]
+                { removeAttrs: {attrs: '(stroke|fill)'} }
+            )
         },
         expand: true,
         cwd: '.tmp/icons-fit/', // __ svg files will be left alone, all other svg files will be completely stripped
         src: ['**/*.svg', '!**/__*.svg'],
         dest: '.tmp/icons-min/'
     },
-    stripAllSome: {
+    minimal: {
         options: {
-            plugins: [
-                { removeViewBox: false, },
-                { removeDimensions: true, },
-                {
-                    cleanupIDs: {
-                        prefix: {
-                            toString() {
-                                this.counter = this.counter || 0;
-
-                                return `id-${this.counter++}`;
-                            }
-                        }
-                    }
-                }
-            ]
+            plugins
         },
         expand: true,
         cwd: '.tmp/icons-fit/',
-        src: ['**/__*.svg'],
-        dest: '.tmp/icons-min/'
-    },
-    stripNone: {
-        options: {
-            plugins: [
-                { removeViewBox: false, },
-                { removeDimensions: true, },
-                {
-                    cleanupIDs: {
-                        prefix: {
-                            toString() {
-                                this.counter = this.counter || 0;
-
-                                return `id-${this.counter++}`;
-                            }
-                        }
-                    }
-                }
-            ]
-        },
-        expand: true,
-        cwd: '.tmp/icons-fit/',
-        src: ['**/--*.svg'],
+        src: ['**/{__,--}*.svg'],
         dest: '.tmp/icons-min/'
     }
 }
