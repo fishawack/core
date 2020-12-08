373d5384ecb2e4220192115d6d30d049dc080bda
Mike Mellor <mike.mellor@f-grp.com>
2020-12-08 12:15:34 +0000

    map include paths to sass task

--- _Tasks/sass.js
+++ _Tasks/sass.js
@@ -3,15 +3,25 @@ module.exports = (grunt) => {
     var path = require('path');
 
     grunt.registerMultiTask('sass', function() {
+        var options = this.options();
+
         var targets = this.files.filter(d => path.basename(d.src[0]).charAt(0) !== '_' && grunt.file.exists(d.src[0]))
-            .map(d => `${d.src[0]}:${d.orig.dest}${path.basename(d.dest, path.extname(d.dest))}.css`).join(' ');
+            .map(d => `${d.src[0]}:${d.orig.dest}${path.basename(d.dest, path.extname(d.dest))}.css`)
+            .join(' ');
+
+        var includes = [
+                'node_modules/breakpoint-sass/stylesheets',
+                'node_modules/normalize-scss/sass',
+                'node_modules/@fishawack/lab-ui/_Build/sass',
+                'node_modules'
+            ]
+            .concat(options.includePaths || [])
+            .map(d => `--load-path=${d}`)
+            .join(' ');
 
         if(targets.length){
             execSync(`sass --update --no-source-map --style=expanded \
-                --load-path=node_modules/breakpoint-sass/stylesheets \
-                --load-path=node_modules/normalize-scss/sass \
-                --load-path=node_modules/@fishawack/lab-ui/_Build/sass \
-                --load-path=node_modules \
+                ${includes} \
                 ${targets}
             `, {encoding: 'utf8', stdio: 'inherit'});
         } else {
