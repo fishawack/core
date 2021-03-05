ec5648d240e5590acf1e9d18be58a040713f26a8
Mike Mellor <mike.mellor@f-grp.com>
2021-03-05 15:10:01 +0000

    schemas now written to new cache folder

--- _Tasks/options/tv4.js
+++ _Tasks/options/tv4.js
@@ -4,16 +4,16 @@ module.exports = {
 			var fs = require('fs-extra');
 
 			try{
-				fs.copySync(`${configPath}_Schema/`, `node_modules/.schema/`);
-				fs.copySync(`_Schema/`, `node_modules/.schema/`);
+				fs.copySync(`${configPath}_Schema/`, `.cache/schema/`);
+				fs.copySync(`_Schema/`, `.cache/schema/`);
 			} catch(e){}
 			
-			return grunt.file.readJSON('node_modules/.schema/schema.json');
+			return grunt.file.readJSON('.cache/schema/schema.json');
 		},
 		schemas: () => {
 			var obj = {};
-			grunt.file.expand({cwd: `node_modules/.schema/`}, '**/*.json').forEach(d => {
-				obj[d] = grunt.file.readJSON(`node_modules/.schema/${d}`);
+			grunt.file.expand({cwd: `.cache/schema/`}, '**/*.json').forEach(d => {
+				obj[d] = grunt.file.readJSON(`.cache/schema/${d}`);
 			});
 			return obj;
 		},
