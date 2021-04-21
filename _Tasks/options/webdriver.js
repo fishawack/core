d37500008b812136160879e749725ddd28e3d10e
Mike Mellor <mike.mellor@f-grp.com>
2021-04-21 12:57:36 +0100

    split webdriver into ui and pdf task / validte now runs ui tests / ui tests looked for in _Test/ui

--- _Tasks/options/webdriver.js
+++ _Tasks/options/webdriver.js
@@ -1,12 +1,19 @@
 module.exports = {
-    default: {
+    options: {
         configFile: '<%= configPath %>wdio.conf.js',
-        specs: [
-            '<%= configPath %>_Node/capture.js'
-        ],
         capabilities: captureEnv().browsers.map((d) => {
         	return {browserName: d};
         }),
         baseUrl: captureEnv().url
+    },
+    default: {
+        specs: [
+            '<%= configPath %>_Node/capture.js'
+        ]
+    },
+    ui: {
+        specs: [
+            '_Test/ui/**/*.js'
+        ]
     }
 }
