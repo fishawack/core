4d2864d0c00052c9512a8106ad34ce1b2754458b
Mike Mellor <mike.mellor@f-grp.com>
2021-11-16 15:27:55 +0000

    updated content test

--- _Test/content.js
+++ _Test/content.js
@@ -6,14 +6,18 @@ const grunt = require('grunt');
 const execSync = require('child_process').execSync;
 const path = require('path');
 const glob = require('glob');
-const opts = {encoding: 'utf8', stdio: 'inherit'};
+const opts = {encoding: 'utf8', stdio: 'pipe'};
 
-describe('capture', () => {
+describe('content', () => {
     before(() => {
         execSync('grunt content --mocha=output', opts);
     });
     
     it('Should pull down assets via lftp', () => {
-        expect(glob.sync(path.join(__dirname, '_fixture/output/_Build/content/**/*'))).to.be.an('array').that.is.not.empty;
+        expect(glob.sync(path.join(__dirname, '_fixture/output/_Build/content/content-0/**/*'))).to.be.an('array').that.is.not.empty;
+    });
+
+    it('Should pull down assets via ftps', () => {
+        expect(glob.sync(path.join(__dirname, '_fixture/output/_Build/content/content-1/**/*'))).to.be.an('array').that.is.not.empty;
     });
 });
