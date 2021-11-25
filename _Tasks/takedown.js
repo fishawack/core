934b6b1ceb61cf986a2edfc59e94c1592cedf0e2
Mike Mellor <mike.mellor@f-grp.com>
2021-11-25 14:16:54 +0000

    takedown also checks for deployValid

--- _Tasks/takedown.js
+++ _Tasks/takedown.js
@@ -1,9 +1,6 @@
 module.exports = grunt => {
     grunt.registerTask('takedown', function(){
-        if(!deployEnv){
-            grunt.log.warn('No deployment configured for ' + deployBranch);
-            return;
-        }
+        if(!deployValid()){return;}
 
         if(deployEnv.ftp){
             grunt.log.warn('Cannot takedown over ftp');
