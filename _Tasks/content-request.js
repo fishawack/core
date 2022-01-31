b09aacc91677758c96a386c4ef4ab9b73aa2d6d9
Mike Mellor <mike.mellor@f-grp.com>
2022-01-31 15:49:52 +0000

    moved content-request to async await format

--- _Tasks/content-request.js
+++ _Tasks/content-request.js
@@ -1,10 +1,11 @@
 module.exports = (grunt) => {
-    grunt.registerTask('content:request', function(){
+    grunt.registerTask('content:request', async function(){
         if(!contentJson.attributes.content || contentJson.attributes.content.length <= 0){
             grunt.log.warn('No content to pull');
             return;
         }
 
+        var done = this.async();
         var request = require('request-promise');
         var fs = require('fs-extra');
         var path = require('path');
@@ -12,15 +13,12 @@ module.exports = (grunt) => {
         var limit = pLimit(5);
         const { download, rewrite } = require('./helpers/requests.js');
 
-        var requests = [];
-        var rewrites = [];
-
-        contentJson.attributes.content.forEach(function(d, i){
+        await Promise.all(contentJson.attributes.content.reduce((arr, d, i) => {
             if(d.url){
-                requests = requests.concat(d.endpoints.map((dd) => limit(() => load({
+                arr = arr.concat(d.endpoints.map((endpoint) => limit(() => load({
                         path: d.url,
                         api: d.api || '/wp-json/wp/v2/',
-                        endpoint: dd,
+                        endpoint,
                         ext: d.ext || 'json',
                         saveTo: (d.saveTo || path.join(config.src, `/content/content-${i}/`, (d.bundle ? 'media/' : '')))
                     }, 1)
@@ -37,55 +35,39 @@ module.exports = (grunt) => {
                         );
                     }))));
             }
-        });
 
-        if(requests.length){
-            var done = this.async();
+            return arr;
+        }, []));
 
-            Promise.all(requests)
-                .then(() => {
-                    let arr = [];
+        // Pull assets referenced in json files
+        await Promise.all(contentJson.attributes.content.map((d, i) => {
+            if(d.url && d.find !== null){
+                return limit(() => download({
+                        path: d.url,
+                        api: d.api || '/wp-json/wp/v2/',
+                        ext: d.ext || 'json',
+                        saveTo: d.saveTo || path.join(config.src, `/content/content-${i}/`),
+                        bundle: d.bundle ? 'media/' : '',
+                        find: d.find || `^https.*/wp-content/uploads`
+                    }));
+            }
+        }));
 
-                    contentJson.attributes.content.forEach(function(d, i){
-                        if(d.url && d.find !== null){
-                            arr.push(limit(() => download({
-                                    path: d.url,
-                                    api: d.api || '/wp-json/wp/v2/',
-                                    ext: d.ext || 'json',
-                                    saveTo: d.saveTo || path.join(config.src, `/content/content-${i}/`),
-                                    bundle: d.bundle ? 'media/' : '',
-                                    find: d.find || `^https.*/wp-content/uploads`
-                                })));
-                        }
-                    });
+        // Rewrite json files
+        await Promise.all(contentJson.attributes.content.map((d, i) => {
+            if(d.url && d.find !== null){
+                return limit(() => rewrite({
+                        path: d.url,
+                        api: d.api || '/wp-json/wp/v2/',
+                        ext: d.ext || 'json',
+                        saveTo: d.saveTo || path.join(config.src, `/content/content-${i}/`),
+                        bundle: d.bundle ? 'media/' : '',
+                        find: d.find || `^https.*/wp-content/uploads`
+                    }));
+            }
+        }));
 
-                    if(arr.length){
-                        Promise.all(arr)
-                            .then(() => {
-                                contentJson.attributes.content.forEach(function(d, i){
-                                    if(d.url && d.find !== null){
-                                        rewrites.push(limit(() => rewrite({
-                                                path: d.url,
-                                                api: d.api || '/wp-json/wp/v2/',
-                                                ext: d.ext || 'json',
-                                                saveTo: d.saveTo || path.join(config.src, `/content/content-${i}/`),
-                                                bundle: d.bundle ? 'media/' : '',
-                                                find: d.find || `^https.*/wp-content/uploads`
-                                            })));
-                                    }
-                                });
-            
-                                Promise.all(rewrites)
-                                    .catch(err => grunt.log.warn(err.message))
-                                    .finally(() => done());
-                            })
-                            .catch(err => grunt.log.warn(err.message));
-                    } else {
-                        done();
-                    }
-                })
-                .catch(err => grunt.log.warn(err.message));
-        }
+        done();
 
         function load(options, index, arr){
             return new Promise((resolve, reject) => {
