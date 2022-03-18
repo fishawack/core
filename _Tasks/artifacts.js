0fc7fee3a9f5d5913f72966c3ba65517cf3480b8
Mike Mellor <mike.mellor@f-grp.com>
2022-03-18 18:14:24 +0000

    artifcats code optimized and now checks if folders exist or throws grunt warn

--- _Tasks/artifacts.js
+++ _Tasks/artifacts.js
@@ -1,24 +1,23 @@
 module.exports = function(grunt) {
     grunt.registerTask('artifacts', () => {
+        const fs = require('fs');
         const lftp = require('./helpers/lftp.js');
         const host = "ftp-fishawack.egnyte.com";
         const creds = config.targets.ftp[host];
         const name = config.pkg.name;
 
-        lftp.push(
-            '_Zips/',
-            `Shared/FW/Knutsford/Digital/Auto-Package/${name}`,
-            creds.username,
-            creds.password,
-            host
-        );
-
-        lftp.push(
-            '_Pdfs/',
-            `Shared/FW/Knutsford/Digital/Auto-Package/${name}`,
-            creds.username,
-            creds.password,
-            host
-        );
+        ['_Zips/', '_Pdfs/'].forEach(d => {
+            if(fs.existsSync(d)){
+                lftp.push(
+                    d,
+                    `Shared/FW/Knutsford/Digital/Auto-Package/${name}`,
+                    creds.username,
+                    creds.password,
+                    host
+                );
+            } else {
+                grunt.log.warn(`No artifacts found in ${d}`);
+            }
+        });
     });
 };
