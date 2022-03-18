ff6ff1feb7e10f84671a56e823da3f19df8f61dc
Mike Mellor <mike.mellor@f-grp.com>
2022-03-18 17:44:39 +0000

    ignore DS_Store from lftp mirror commands

--- _Tasks/helpers/lftp.js
+++ _Tasks/helpers/lftp.js
@@ -5,12 +5,14 @@ function command(command, username, password, host){
     execSync(`lftp -e '${command}; exit;' -u '${username}','${password}' sftp://${host}`, opts);
 };
 
+const mirror = command => `set sftp:auto-confirm yes; mirror ${command} --exclude-glob *.DS_Store -p --parallel=10`
+
 module.exports = {
     pull(local, server, username, password, host){
-        command(`set sftp:auto-confirm yes; mirror ${server} ${local} -p --parallel=10`, username, password, host);
+        command(mirror(`${server} ${local}`), username, password, host);
     },
     push(local, server, username, password, host){
-        command(`set sftp:auto-confirm yes; mirror -R ${local} ${server} -p --parallel=10`, username, password, host);
+        command(mirror(`-R ${local} ${server}`), username, password, host);
     },
     remove(server, username, password, host){
         command(`rm ${server}`, username, password, host);
