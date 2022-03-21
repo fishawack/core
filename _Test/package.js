4e627b536b1b469818bdb1643cb814d5fd499972
Mike Mellor <mike.mellor@f-grp.com>
2022-03-21 12:09:59 +0000

    test suite for package task

--- _Test/package.js
+++ _Test/package.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const glob = require('glob');
 const expect = require('chai').expect;
 const execSync = require('child_process').execSync;
@@ -10,12 +11,15 @@ describe('package', () => {
     it('Should not generate an app zip if app flag not preset on branch', () => {
         execSync('grunt clean:zip clean:pdf package --mocha=package', opts);
 
-        expect(glob.sync(path.join(__dirname, '_fixture/output/_Zips/*_App.zip'))).to.be.an('array').that.is.empty;
+        expect(glob.sync(path.join(__dirname, '_fixture/package/_Zips/*_App.zip'))).to.be.an('array').that.is.empty;
+
+        expect(fs.existsSync(path.join(__dirname, '_fixture/package/_Zips'))).to.be.false;
+        expect(fs.existsSync(path.join(__dirname, '_fixture/package/_Pdfs'))).to.be.false;
     });
 
     it('Should generate an app zip if app flag not preset on branch', () => {
         execSync('grunt clean:zip package --mocha=package --branch=package', opts);
 
-        expect(glob.sync(path.join(__dirname, '_fixture/output/_Zips/*_App.zip'))).to.be.an('array').that.is.not.empty;
+        expect(glob.sync(path.join(__dirname, '_fixture/package/_Zips/*_App.zip'))).to.be.an('array').that.is.not.empty;
     });
 });
