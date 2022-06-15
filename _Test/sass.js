c08a6afa096ad5e61e9ef5911e5e4695737d6784
Mike Mellor <mike.mellor@f-grp.com>
2022-06-15 12:03:35 +0100

    reworked expected file names and test descriptions to be more clear

--- _Test/sass.js
+++ _Test/sass.js
@@ -7,37 +7,37 @@ const path = require('path');
 const { opts } = require('./_helpers/globals.js');
 
 describe('sass', () => {
-    let css;
+    it('Should generate a css bundle', () => {
+        execSync('grunt sass:default --branch=master --mocha=bundle', opts);
 
-    before(() => {
-        execSync('grunt clean:cache compile-handlebars:default htmlmin:default sass:default --dist --mocha=sass', opts);
-    });
-    
-    it('Should generate a css file in the _Output directory', () => {
         try{
-            css = fs.readFileSync(path.join(__dirname, '_fixture/sass/_Output/css/general.css'), opts);
+            fs.readFileSync(path.join(__dirname, '_fixture/bundle/_Output/css/general.css'), opts);
         } catch(e){
             expect(e.message).to.not.contain('ENOENT');
         }
     });
 
-    it('Should match expected css output', () => {
-        expect(css).to.equal(fs.readFileSync(path.join(__dirname, '_expected/general.css'), opts));
-    });
-
-    it('Should not run postcss on branches that do not have a deploy object on the target branch', () => {
+    it('Should not run postcss', () => {
         execSync('grunt clean:cache compile-handlebars:default htmlmin:default sass:default --mocha=sass', opts);
 
         let css = fs.readFileSync(path.join(__dirname, '_fixture/sass/_Output/css/general.css'), opts);
         
-        expect(css).to.equal(fs.readFileSync(path.join(__dirname, '_expected/general-no-dist.css'), opts));
+        expect(css).to.equal(fs.readFileSync(path.join(__dirname, '_expected/general.css'), opts));
     });
 
-    it('Should run postcss on branches that do have a deploy object on the target branch', () => {
+    it('Should run postcss on builds with dist flag', () => {
+        execSync('grunt clean:cache compile-handlebars:default htmlmin:default sass:default --dist --mocha=sass', opts);
+
+        let css = fs.readFileSync(path.join(__dirname, '_fixture/sass/_Output/css/general.css'), opts);
+
+        expect(css).to.equal(fs.readFileSync(path.join(__dirname, '_expected/general-dist.css'), opts));
+    });
+
+    it('Should run postcss but not minify on branches that have a deploy target without a dist flag', () => {
         execSync('grunt clean:cache compile-handlebars:default htmlmin:default sass:default --mocha=sass --branch=postcss', opts);
 
         let css = fs.readFileSync(path.join(__dirname, '_fixture/sass/_Output/css/general.css'), opts);
         
-        expect(css).to.equal(fs.readFileSync(path.join(__dirname, '_expected/general-no-dist-purgecss.css'), opts));
+        expect(css).to.equal(fs.readFileSync(path.join(__dirname, '_expected/general-no-dist-with-target.css'), opts));
     });
 });
