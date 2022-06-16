f9cecdc0aea43cb839b5db7683670f56865e9361
Mike Mellor <mike.mellor@f-grp.com>
2022-06-16 20:46:06 +0100

    reset cwd after export tests

--- _Test/exports.js
+++ _Test/exports.js
@@ -5,4 +5,8 @@ describe('captureEnv', () =>{
         require('../_Tasks/helpers/include.js')(require('grunt'), false, 'bundle');
         expect(captureEnv().url).to.be.equal('http://localhost:9001');
     });
+
+    after(() => {
+        process.chdir('/app');
+    });
 });
