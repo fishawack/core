f4842cf0cd880dda526bc8e459e11a48e62b502c
Mike Mellor <mike.mellor@f-grp.com>
2022-06-17 18:55:34 +0100

    tv4 tests now globs all fixtures

--- _Test/tv4.js
+++ _Test/tv4.js
@@ -3,13 +3,15 @@
 const expect = require('chai').expect;
 const execSync = require('child_process').execSync;
 const { opts } = require('./_helpers/globals.js');
+const glob = require('glob');
+const path = require('path');
 
 describe('tv4', () => {
     it('Should pass tv4 linting', async () => {
         try{
-            execSync('grunt tv4 --branch=master --mocha=bundle', opts);
-            execSync('grunt tv4 --branch=master --mocha=output', opts);
-            execSync('grunt tv4 --branch=master --mocha=reload', opts);
+            glob.sync('*', {cwd: path.join(__dirname, `/_fixture`)}).forEach(fixture => {
+                execSync(`grunt tv4 --branch=master --mocha=${fixture}`, opts); 
+            });
         } catch(e){
             expect(e.message).to.not.contain('Command failed');
         }
