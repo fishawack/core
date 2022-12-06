466830c04e4122abb672422530f00b6e01e142d6
Mike Mellor <mike.mellor@f-grp.com>
2022-12-06 16:07:29 +0000

    test: added electron test

---
+++ _Test/electron.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const expect = require("chai").expect;
+const execSync = require("child_process").execSync;
+const path = require("path");
+const glob = require("glob");
+const { opts } = require("./_helpers/globals.js");
+
+describe("electron", () => {
+  before(() => {
+    execSync("grunt package:electron --branch=package --mocha=package", opts);
+  });
+
+  it("Should build a macOS electron package", () => {
+    expect(glob.sync(path.join(__dirname, '_fixture/package/_Packages/Electron/**/*.app'))).to.be.an('array').to.not.be.empty;
+  });
+
+  it("Should build a Windows electron package", () => {
+    expect(glob.sync(path.join(__dirname, '_fixture/package/_Packages/Electron/**/*.exe'))).to.be.an('array').to.not.be.empty;
+  });
+});
