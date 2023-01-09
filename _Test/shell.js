873f7ea198b1cb9a47ba4fea238e165a68d706f3
Mike Mellor <mike.mellor@f-grp.com>
2023-01-09 12:19:54 +0000

    feat: shell task now custom

---
+++ _Test/shell.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const expect = require('chai').expect;
+const execSync = require('child_process').execSync;
+const shell = require('../_Tasks/shell.js').shell;
+
+describe('shell', () => {
+    it('Should echo to terminal when called directly', () => {
+        expect(shell('echo hello', {stdio: 'pipe'})).to.include('hello');
+    });
+
+    it('Should echo to terminal when called as task', () => {
+        expect(execSync('grunt shell:test --mocha=output', {encoding: 'utf-8'})).to.include('hello');
+    });
+
+    it('Should throw error when error in command', () => {
+        expect(() => shell('yabbadabba', {stdio: 'pipe'})).to.throw();
+    });
+
+    it('Should not throw error when failOnError set to false and error in command', () => {
+        expect(() => shell('yabbadabba', {stdio: 'pipe'}, {failOnError: false})).to.not.throw();
+    });
+});
