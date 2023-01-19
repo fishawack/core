594b2811b9f4504f729652dfbef01190f6856baa
Mike Mellor <mike.mellor@f-grp.com>
2023-01-19 17:43:32 +0000

    test: added fontello-svg tests

--- _Test/svg.js
+++ _Test/svg.js
@@ -4,18 +4,35 @@ const fs = require('fs');
 const expect = require('chai').expect;
 const execSync = require('child_process').execSync;
 const path = require('path');
+const glob = require("glob");
 const { opts } = require('./_helpers/globals.js');
 
 describe('svg', () => {
-    before(() => {
-        execSync('grunt fontello_svg svgfit svgmin svg_sprite --branch=master --mocha=bundle', opts);
+    describe('fontello-svg', () => {
+        before(() => {
+            execSync('grunt fontello_svg --branch=master --mocha=bundle', opts);
+        });
+        
+        it('Should download icons from fontello.com as svgs', () => {
+            expect(glob.sync(path.join(__dirname, '_fixture/bundle/_Build/icons/generated/*.svg'))).to.be.an('array').that.is.not.empty;
+        });
+
+        it('Should download icons with custom file format', () => {
+            expect(glob.sync(path.join(__dirname, '_fixture/bundle/_Build/icons/generated/spin6.svg'))).to.be.an('array').that.is.not.empty;
+        });
     });
-    
-    it('Should generate svgSprite.svg', () => {
-        try{
-            fs.readFileSync(path.join(__dirname, '_fixture/bundle/_Build/handlebars/generated/svgSprite.svg'), opts);
-        } catch(e){
-            expect(e.message).to.not.contain('ENOENT');
-        }
+
+    describe('svgsprite', () => {
+        before(() => {
+            execSync('grunt fontello_svg svgfit svgmin svg_sprite --branch=master --mocha=bundle', opts);
+        });
+        
+        it('Should generate svgSprite.svg', () => {
+            try{
+                fs.readFileSync(path.join(__dirname, '_fixture/bundle/_Build/handlebars/generated/svgSprite.svg'), opts);
+            } catch(e){
+                expect(e.message).to.not.contain('ENOENT');
+            }
+        });
     });
 });
