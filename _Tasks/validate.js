4daf93e386f1ad93607155278cc7e086b5643547
Mike Mellor <mike.mellor@f-grp.com>
2023-02-08 18:22:50 +0000

    fix: remove badges call in validate

--- _Tasks/validate.js
+++ _Tasks/validate.js
@@ -1,3 +1,3 @@
 module.exports = grunt => { 
-    grunt.registerTask('validate', ['jshint', 'tv4', 'karma:continuous', 'connect', 'webdriver:ui', 'badges', 'coverage']);
+    grunt.registerTask('validate', ['jshint', 'tv4', 'karma:continuous', 'connect', 'webdriver:ui', 'coverage']);
 };
