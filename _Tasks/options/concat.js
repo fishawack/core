4e56c7a6cf059f19fea2d8e841441158257b6f34
Mike Mellor <mike.mellor@f-grp.com>
2023-02-08 13:05:38 +0000

    refactor: remove unneeded webpack ++ concat as the logic is in Gruntfile.js

--- _Tasks/options/concat.js
+++ _Tasks/options/concat.js
@@ -6,32 +6,12 @@ module.exports = {
         files: [
         {
             '<%= root %>/js/crucial.js': ['<%= src %>/js/**/__*.js']
-        },
-        {
-            expand: true,
-            cwd: '<%= src %>/js/',
-            src: ['**/++*.js'],
-            dest: '<%= root %>/js/',
-            flatten: true,
-            rename: function(path, file){
-                return path + file.replace('++', '');
-            }
         }]
     },
     dist: {
         files: [
         {
             '.tmp/js/crucial.js': ['<%= src %>/js/**/__*.js']
-        },
-        {
-            expand: true,
-            cwd: '<%= src %>/js/',
-            src: ['**/++*.js'],
-            dest: '.tmp/js/',
-            flatten: true,
-            rename: function(path, file){
-                return path + file.replace('++', '');
-            }
         }]
     }
 }
