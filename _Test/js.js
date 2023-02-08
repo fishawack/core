ae407563c0edfec952db29b4b596ba47eb1c479f
Mike Mellor <mike.mellor@f-grp.com>
2023-02-08 13:05:19 +0000

    test: added more js tests

--- _Test/js.js
+++ _Test/js.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const expect = require('chai').expect;
 const execSync = require('child_process').execSync;
 const path = require('path');
+const glob = require('glob');
 const { opts } = require('./_helpers/globals.js');
 
 describe('js', () => {
@@ -12,10 +13,58 @@ describe('js', () => {
     });
     
     it('Should generate a javascript bundle', () => {
-        try{
-            fs.readFileSync(path.join(__dirname, '_fixture/bundle/_Output/js/script.js'), opts);
-        } catch(e){
-            expect(e.message).to.not.contain('ENOENT');
-        }
+        expect(glob.sync(path.join(__dirname, '_fixture/bundle/_Output/js/script.js'))).to.be.an('array').that.is.not.empty;
+    });
+
+    describe('entrypoints', () => {
+        it('Should generate c.js when file with -- prefix is found', () => {
+            expect(glob.sync(path.join(__dirname, '_fixture/bundle/_Output/js/c.js'))).to.be.an('array').that.is.not.empty;
+        });
+    });
+
+    describe('concat', () => {
+        it('Should generate crucial.js when files with __ prefix are found', () => {
+            expect(glob.sync(path.join(__dirname, '_fixture/bundle/_Output/js/crucial.js'))).to.be.an('array').that.is.not.empty;
+        });
+
+        it('Should generate b.js when files with ++ prefix are found', () => {
+            expect(glob.sync(path.join(__dirname, '_fixture/bundle/_Output/js/crucial.js'))).to.be.an('array').that.is.not.empty;
+        });
+
+        describe('crucial', () => {
+            let js;
+
+            before(() => {
+                js = fs.readFileSync(path.join(__dirname, '_fixture/bundle/_Output/js/crucial.js'), {encoding: 'utf8'});
+            });
+
+            it('Should concat __a.js into crucial.js', () => {
+                expect(js).contain('"__a.js"');
+            });
+
+            it('Should concat __z.js into crucial.js', () => {
+                expect(js).contain('"__z.js"');
+            });
+
+            it('Should concat __modernizr--custom.js into crucial.js', () => {
+                expect(js).contain('modernizr');
+            });
+        });
+
+        describe('dynamic', () => {
+            let js;
+
+            before(() => {
+                js = fs.readFileSync(path.join(__dirname, '_fixture/bundle/_Output/js/b.js'), {encoding: 'utf8'});
+            });
+
+            it('Should concat ++b.1.js into b.js', () => {
+                expect(js).contain('"++b.1.js"');
+            });
+
+            it('Should concat ++b.1.js into b.js', () => {
+                expect(js).contain('"++b.2.js"');
+            });
+        });
     });
 });
