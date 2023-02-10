64502da34c8085610e67026454194333543e7c01
Mike Mellor <mike.mellor@f-grp.com>
2023-02-10 17:03:33 +0000

    test: removed compare tests from pdf as those are now in dedicated test file

--- _Test/pdf.js
+++ _Test/pdf.js
@@ -16,17 +16,4 @@ describe('pdf', () => {
 
         expect(glob.sync(path.join(__dirname, '_fixture/capture/_Pdfs/*_chrome.pdf'))).to.be.an('array').that.is.not.empty;
     });
-
-    it('Should compare browsers', () => {
-        execSync('grunt compare:browsers --branch=master --mocha=capture', opts);
-
-        expect(glob.sync(path.join(__dirname, '_fixture/capture/_Build/media/generated/__chrome - chrome.png'))).to.be.an('array').that.is.not.empty;
-        expect(glob.sync(path.join(__dirname, '_fixture/capture/_Build/media/generated/__chrome - chrome.svg'))).to.be.an('array').that.is.not.empty;
-    });
-
-    it('Should compare the latest build against the last', () => {
-        execSync('grunt shell:pullPrevious compare:previous shell:pushPrevious --branch=master --mocha=capture', opts);
-
-        expect(glob.sync(path.join(__dirname, '_fixture/capture/_Pdfs/*_chrome_compare.pdf'))).to.be.an('array').that.is.not.empty;
-    });
 });
