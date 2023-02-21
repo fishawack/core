9fb7f9edefc87be1b9f5850ff2f9ef4e15fd16c3
Mike Mellor <mike.mellor@f-grp.com>
2023-02-21 17:06:06 +0000

    test: added realistic js boilerplate to simulate polyfills needed

--- _Test/_fixture/bundle/_Build/js/script.js
+++ _Test/_fixture/bundle/_Build/js/script.js
@@ -1,4 +1,22 @@
 "use strict";
 
-document.querySelector('html').classList.remove('loading');
-document.querySelector('html').classList.add('loaded');
+"use strict";
+
+import "./libs/build-id.js";
+
+import * as Utility from "./libs/utility";
+
+(function(){
+	if(navigator.userAgent === 'jsdom'){ return; }
+
+    // Used for capturing pdf as part of preprocess deploy
+	if (Utility.parse_query_string(window.location.search.substring(1)).capture === 'true') {
+		document.querySelector('html').classList.add('capture');
+		window.capture = true;
+	}
+
+	Utility.styleguide();
+
+	document.querySelector('html').classList.remove('loading');
+	document.querySelector('html').classList.add('loaded');
+})();
