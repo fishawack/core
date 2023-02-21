28fff701615702fbfe5216486c3ebea3322ece10
Mike Mellor <mike.mellor@f-grp.com>
2023-02-21 13:12:51 +0000

    fix: electron now requires hidenavbar to be set explicitly

--- _Node/electron.js
+++ _Node/electron.js
@@ -11,7 +11,7 @@ function createWindow () {
 	var screen = electron.screen.getPrimaryDisplay().bounds;
 
 	// Create the browser window.
-	win = new BrowserWindow({width: screen.width, height: screen.height, resizable: false, fullscreen: true})
+	win = new BrowserWindow({width: screen.width, height: screen.height, resizable: false, fullscreen: true, autoHideMenuBar: true})
 
 	// and load the index.html of the app.
 	win.loadURL(url.format({
