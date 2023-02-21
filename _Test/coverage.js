acac0187e7e72810235f3d94b54da46948a9b5bd
Mike Mellor <mike.mellor@f-grp.com>
2023-02-21 22:02:25 +0000

    test: coverage now tests real coverage

--- _Test/coverage.js
+++ _Test/coverage.js
@@ -8,6 +8,12 @@ const { opts } = require('./_helpers/globals.js');
 
 describe('coverage', () => {    
     it('Should output the code coverage to terminal', () => {
+        let output = execSync('grunt karma coverage --mocha=test', {encoding: 'utf8'});
+
+        expect(output).to.contain('Code coverage: 87.5%');
+    });
+
+    it('Should output the code coverage to terminal with no tests', () => {
         let output = execSync('grunt coverage --mocha=bundle --branch=master', {encoding: 'utf8'});
 
         expect(output).to.contain('Code coverage: 0.0%');
