56375bab6556765644e4d69849618522524e30c8
Mike Mellor <mike.mellor@f-grp.com>
2023-02-21 19:46:25 +0000

    refactor: imagemin no longer runs as part of dist

--- _Tasks/dist.js
+++ _Tasks/dist.js
@@ -1,5 +1,5 @@
 module.exports = grunt => {
-    var arr = ['clean:cache', 'clean:dist', 'modernizr', 'json-schema', 'fontello_svg', 'svgfit', 'svgmin', 'svg_sprite', 'copy:content', 'copy:shared', 'copy:assets', 'copy:svg', 'copy:svgasis', 'webpack:dist', 'concat:dist', 'uglify:dist', 'compile-handlebars', 'htmlmin', 'sass', 'cacheBust', 'imagemin', 'clean:build'];
+    var arr = ['clean:cache', 'clean:dist', 'modernizr', 'json-schema', 'fontello_svg', 'svgfit', 'svgmin', 'svg_sprite', 'copy:content', 'copy:shared', 'copy:assets', 'copy:svg', 'copy:svgasis', 'webpack:dist', 'concat:dist', 'uglify:dist', 'compile-handlebars', 'htmlmin', 'sass', 'cacheBust', 'clean:build'];
 
     /* PreRender */
     if(contentJson.attributes.prerender){
