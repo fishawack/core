d26525aa6141e2611ba1a447bebfcad070a2729f
Mike Mellor <mike.mellor@f-grp.com>
2023-02-26 11:18:04 +0000

    test: export misc creds from globals helper

--- _Test/_helpers/globals.js
+++ _Test/_helpers/globals.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const os = require('os');
 const creds = JSON.parse(fs.readFileSync(`${os.homedir()}/targets/ftp-fishawack.egnyte.com.json`));
+const misc = JSON.parse(fs.readFileSync(`${os.homedir()}/targets/misc.json`));
 
 module.exports = {
     opts: {encoding: 'utf8', stdio: process.argv.includes('--publish') ? 'pipe' : 'inherit'},
-    creds
+    creds,
+    misc
 };
