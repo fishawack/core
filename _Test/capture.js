338942c782f2830bd7df7a340048f9ae2d0d535d
Mike Mellor <mike.mellor@f-grp.com>
2023-03-21 18:40:35 +0000

    test: more accurate path tests in capture generation

--- _Test/capture.js
+++ _Test/capture.js
@@ -13,15 +13,15 @@ describe('capture', () => {
         });
 
         it('Should capture index.html page if no pages property defined', () => {
-            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/**/*_indexhtml_.png'))).to.be.an('array').that.is.not.empty;
+            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/**/*x*/*_indexhtml_.png'))).to.be.an('array').that.is.not.empty;
         });
 
         it('Should capture all html routes found in root folder', () => {
-            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/**/*.png')).map(d => path.basename(d))).to.have.members(['0_abouthtml_.png', '1_faqindexhtml_.png', '2_indexhtml_.png', '3_longhtml_.png']);
+            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/**/*x*/*.png')).map(d => path.basename(d))).to.have.members(['0_abouthtml_.png', '1_faqindexhtml_.png', '2_indexhtml_.png', '3_longhtml_.png']);
         });
     
         it('Should capture chrome if no browser property defined', () => {
-            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/chrome/**/*.png'))).to.be.an('array').that.is.not.empty;
+            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/chrome/**/*x*/*.png'))).to.be.an('array').that.is.not.empty;
         });
     
         it('Should capture 1080x608 if sizes property not defined', () => {
@@ -40,8 +40,8 @@ describe('capture', () => {
         });
     
         it('Should capture only browsers defined in config', () => {
-            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/firefox/**/*.png'))).to.be.an('array').that.is.empty;
-            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/chrome/**/*.png'))).to.be.an('array').that.is.not.empty;
+            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/firefox/**/*x*/*.png'))).to.be.an('array').that.is.empty;
+            expect(glob.sync(path.join(__dirname, '_fixture/capture/.tmp/screenshots/chrome/**/*x*/*.png'))).to.be.an('array').that.is.not.empty;
         });
     
         it('Should capture only sizes defined in config', () => {
