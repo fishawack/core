717433dbaaf34f9fba3dac24495c4ff6557bbc08
Mike Mellor <mike.mellor@f-grp.com>
2023-03-27 15:07:27 +0100

    fix: switched image-size for sharp so we don't have dupe dependencies

--- _Node/pdfPack.js
+++ _Node/pdfPack.js
@@ -1,16 +1,16 @@
 const PDFDocument = require('pdfkit');
-const sizeOf = require('image-size');
+const sharp = require('sharp');
 const fs = require('fs');
 
 module.exports = async (pages, dest) => {
-    return new Promise((resolve) => {
+    return new Promise(async (resolve) => {
         const doc = new PDFDocument({ autoFirstPage: false });
 
         const out = fs.createWriteStream(dest);
         doc.pipe(out);
 
         for (let i = 0; i < pages.length; i++) {
-            let size = sizeOf(pages[i]);
+            const size = await sharp(pages[i]).metadata();
             
             doc.addPage({size: [size.width, size.height]});
 
