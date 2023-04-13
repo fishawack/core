6db0eebab8a27903fb10c7d65ec5a98dc83ed3b3
Irina Hogg <ihogg@dudnyk.com>
2023-04-13 16:48:25 -0400

    feat: update veeva test to check for csv contents WIP

--- _Test/veeva.js
+++ _Test/veeva.js
@@ -5,6 +5,7 @@ const execSync = require('child_process').execSync;
 const path = require('path');
 const glob = require('glob');
 const { opts } = require('./_helpers/globals.js');
+const fs = require('fs-extra');
 
 describe('veeva', () => {
     before(() => {
@@ -23,6 +24,13 @@ describe('veeva', () => {
         expect(glob.sync(path.join(__dirname, '_fixture/package/_Packages/Veeva/veeva_shared_resource.zip'))).to.be.an('array').to.be.empty;
     });
 
+    // it('Should generate a csv', () => {
+    //     expect(glob.sync(path.join(__dirname, '_fixture/package/_Packages/Veeva/MCL*.csv'))).to.be.an('array').to.not.be.empty;
+    // });
+    // it('Should find language in csv', () => {
+    //     expect(fs.readFileSync(path.join(__dirname, '_fixture/package/_Packages/Veeva/MCL*.csv'), { encoding: 'utf8' })).to.contain(`English`);
+    // });
+
     describe('sharedResource', () => {
         describe('not generated', () => {
             before(() => {
@@ -38,7 +46,7 @@ describe('veeva', () => {
             before(() => {
                 execSync('mkdir -p _Test/_fixture/package/_Output/shared');
                 execSync('touch _Test/_fixture/package/_Output/shared/shared.css');
-                execSync('grunt clean:veeva veeva --branch=veeva-shared-resource --mocha=package', opts);
+                execSync('grunt capture package:veeva --branch=veeva-shared-resource --mocha=package', opts);
             });
     
             it('Should generate a shared resource zip when flag enabled', () => {
@@ -51,6 +59,14 @@ describe('veeva', () => {
                 expect(glob.sync(path.join(__dirname, '_fixture/package/_Packages/Veeva/veeva_shared_resource/thumb.png'))).to.be.an('array').to.not.be.empty;
                 expect(glob.sync(path.join(__dirname, '_fixture/package/_Packages/Veeva/veeva_shared_resource/index.html'))).to.be.an('array').to.not.be.empty;
             });
+
+            it('Should generate a csv', () => {
+                expect(glob.sync(path.join(__dirname, '_fixture/package/_Packages/Veeva/MCL*veeva-shared-resource*.csv'))).to.be.an('array').to.not.be.empty;
+            });
+            it('Should find language in csv', () => {
+                expect(fs.readFileSync(path.join(__dirname, '_fixture/package/_Packages/Veeva/MCL*veeva-shared-resource*.csv'), { encoding: 'utf8' })).to.contain(`English`);
+            });
+
 
             after(() => {
                 execSync('rm -rf _Test/_fixture/package/_Output/shared');
