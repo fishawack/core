56085d81503e632858074447fd1d1e7b1de7d71f
Chris Palmer <cpalmer@dudnyk.com>
2023-05-02 11:37:55 -0400

    feat: adding branch detection to pre-commit hook, will reject commit if on the master branch

--- _Test/githooks.js
+++ _Test/githooks.js
@@ -25,7 +25,31 @@ describe('githooks', () => {
         cmd(`git init`);
         cmd(`git config user.email "you@example.com" && git config user.name "Your Name"`);
         cmd(`git config core.hooksPath ${path.join(__dirname, '../.githooks')}`, true);
+        cmd(`git config safe.directory '*'`);
+        cmd(`git checkout -b 'development'`);
         deleteFiles(['_Build']);
+    });
+
+    it('should  pass pre-commit hook when pushing to non-master brances', () => {
+        createFiles(['_Build/config/level-0.json']);
+        cmd(`git checkout -b 'development'`);
+        cmd(`git add .`);
+        expect(() => cmd(`git commit -m "test"`)).to.not.throw();
+        createFiles(['_Build/config/level-1.json']);
+        cmd(`git checkout -b 'staging'`);
+        cmd(`git add .`);
+        expect(() => cmd(`git commit -m "test"`)).to.not.throw();
+        createFiles(['_Build/config/level-2.json']);
+        cmd(`git checkout -b 'master-alt'`);
+        cmd(`git add .`);
+        expect(() => cmd(`git commit -m "test"`)).to.not.throw();
+    });
+    
+    it('should not pass pre-commit hook when pushing to master', () => {
+        createFiles(['_Build/config/level-0.json']);
+        cmd(`git checkout -b 'master'`);
+        cmd(`git add .`);
+        expect(() => cmd(`git commit -m "test"`)).to.throw();
     });
     
     it('should pass pre-commit hook if only level-0 file is committed', () => {
@@ -33,6 +57,7 @@ describe('githooks', () => {
         cmd(`git add .`);
         expect(() => cmd(`git commit -m "test"`)).to.not.throw();
     });
+
     it('should not pass pre-commit hook if level-0 and level-1 files are committed', () => {
         createFiles([
             '_Build/config/level-0.json', 
@@ -41,6 +66,7 @@ describe('githooks', () => {
         cmd(`git add .`);
         expect(() => cmd(`git commit -m "test"`)).to.throw();
     });
+
     it('should pass pre-commit hook if only current-level files are committed', () => {
         createFiles([
             '_Build/config/level-0.json'
@@ -58,6 +84,7 @@ describe('githooks', () => {
         ]);
         expect(() => cmd(`git add . && git commit -m "commit level 2 files"`)).to.not.throw();
     });
+
     it('should not pass pre-commit hook if low-level files are committed with current-level files', () => {
         createFiles([
             '_Build/config/level-1.json', 
@@ -68,6 +95,7 @@ describe('githooks', () => {
         cmd(`git add .`);
         expect(() => cmd(`git commit -m "test"`)).to.throw();
     });
+
     afterEach(() => {
         // remove the .git folder from the fixture folder (so it doesn't get committed)
         execSync(`rm -rf ${path.join(__dirname, '_fixture/githooks/.git')}`, opts);
