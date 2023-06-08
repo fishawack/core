c9cbd2c961f595e4f40fcbb013337fcc459df41e
Mike Mellor <mike.mellor@f-grp.com>
2023-06-08 15:16:44 +0100

    test: only run schema test against fixtures with valid repo

--- _Test/json-schema.js
+++ _Test/json-schema.js
@@ -5,12 +5,15 @@ const execSync = require('child_process').execSync;
 const { opts } = require('./_helpers/globals.js');
 const glob = require('glob');
 const path = require('path');
+const fs = require('fs-extra');
 
 describe('json-schema', () => {
     it('Should pass json schema validation', async () => {
         try{
             glob.sync('*', {cwd: path.join(__dirname, `/_fixture`)}).forEach(fixture => {
-                execSync(`grunt json-schema --branch=master --mocha=${fixture}`, opts); 
+                if(fs.existsSync(path.join(__dirname, `/_fixture`, fixture, 'package.json'))){
+                    execSync(`grunt json-schema --branch=master --mocha=${fixture}`, opts); 
+                }
             });
         } catch(e){
             expect(e.message).to.not.contain('Command failed');
