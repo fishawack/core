8069fab35625d892ccc01f15ab3c12cd8c0d2f09
Mike Mellor <mike.mellor@f-grp.com>
2024-01-19 17:12:38 +0000

    refactor: export multiple tasks at all times and use names

--- _Tasks/package.js
+++ _Tasks/package.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-    grunt.registerTask('package', module.exports.task);
+    grunt.registerTask('package', module.exports.tasks.package);
 };
 
 module.exports.packages = [
@@ -35,38 +35,40 @@ module.exports.packages = [
     }
 ];
 
-module.exports.task = function(){
-    const { isWatertight } = require('./helpers/include.js');
-
-    const package = ['clean:zip'];
-
-    const requested = module.exports.packages.filter(d => contentJson.attributes[d.property]);
-
-    /* CAPTURE */
-    if(requested.filter(d => d.capture).length){
-        package.push('capture');
+module.exports.tasks = {
+    package() {
+        const { isWatertight } = require('./helpers/include.js');
+    
+        const package = ['clean:zip'];
+    
+        const requested = module.exports.packages.filter(d => contentJson.attributes[d.property]);
+    
+        /* CAPTURE */
+        if(requested.filter(d => d.capture).length){
+            package.push('capture');
+        }
+    
+        package.push(
+            ...requested.reduce(
+                (arr, b) =>
+                    arr.concat(
+                        [`package:${b.property}`].concat(
+                            (b.zips || [b.property]).map((d) => `compress:${d}`)
+                        )
+                    ),
+                []
+            )
+        );
+    
+        /* AUTO-PACKAGE */
+        package.push('artifacts');
+    
+        /* WATERTIGHT */
+        isWatertight(deployEnv.loginType) ? package.push('package:watertight', 'compress:watertight') : grunt.log.warn('No watertight specified');
+    
+        /* DEPLOY */
+        deployEnv ? package.push('package:deploy') : grunt.log.warn('No deploy packaging specified');
+    
+        grunt.task.run(package);
     }
-
-    package.push(
-        ...requested.reduce(
-            (arr, b) =>
-                arr.concat(
-                    [`package:${b.property}`].concat(
-                        (b.zips || [b.property]).map((d) => `compress:${d}`)
-                    )
-                ),
-            []
-        )
-    );
-
-    /* AUTO-PACKAGE */
-    package.push('artifacts');
-
-    /* WATERTIGHT */
-    isWatertight(deployEnv.loginType) ? package.push('package:watertight', 'compress:watertight') : grunt.log.warn('No watertight specified');
-
-    /* DEPLOY */
-    deployEnv ? package.push('package:deploy') : grunt.log.warn('No deploy packaging specified');
-
-    grunt.task.run(package);
 };
