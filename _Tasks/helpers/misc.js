66a8b92de12eae9840228dc5d2cc0daf1a2897d3
Mike Mellor <mike.mellor@f-grp.com>
2024-01-19 18:24:10 +0000

    feat: added capitalize and moved package types to misc

--- _Tasks/helpers/misc.js
+++ _Tasks/helpers/misc.js
@@ -1,7 +1,73 @@
 function url_join() {
-    return new URL(require('path').join(...[].slice.call(arguments, 1)), arguments[0]).toString();
-};
+    return new URL(
+        require("path").join(...[].slice.call(arguments, 1)),
+        arguments[0]
+    ).toString();
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const packages = [
+    {
+        name: "pdf",
+        capture: true,
+        zips: [],
+    },
+    {
+        name: "app",
+        zips: [
+            {
+                cwd: "_Output/",
+            },
+        ],
+    },
+    {
+        name: "handover",
+    },
+    {
+        name: "veeva",
+        capture: true,
+        zips: [
+            {
+                src: ["**", "!**/ctlfile/**/*", "!**/ctlfile"],
+            },
+        ],
+    },
+    {
+        name: "cegedim",
+        capture: true,
+    },
+    {
+        name: "vablet",
+    },
+    {
+        name: "electron",
+        zips: [
+            {
+                name: "mac",
+                cwd: "_Packages/Electron/<%= repo.name %>-darwin-x64/",
+            },
+            {
+                name: "win",
+                cwd: "_Packages/Electron/<%= repo.name %>-win32-x64/",
+            },
+        ],
+    },
+    {
+        name: "phonegap",
+        zips: [
+            {
+                name: "ios",
+                src: ["app.ipa"],
+            },
+        ],
+    },
+];
 
 module.exports = {
-    url_join
-}
+    url_join,
+    packages,
+    capitalize,
+};
