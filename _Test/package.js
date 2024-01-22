bf42f543ef1fab489bbbf3c5d88493867e900f82
Mike Mellor <mike.mellor@f-grp.com>
2024-01-22 16:04:00 +0000

    test: made package test dynamic using hte misc object

--- _Test/package.js
+++ _Test/package.js
@@ -7,6 +7,7 @@ const execSync = require('child_process').execSync;
 const path = require('path');
 const { opts, creds } = require('./_helpers/globals.js');
 const lftp = require('../_Tasks/helpers/lftp.js');
+const { packages, capitalize } = require('../_Tasks/helpers/misc.js');
 
 describe('package', () => {    
     it('Should not generate an app zip if app flag not preset on branch', () => {
@@ -18,10 +19,22 @@ describe('package', () => {
         expect(fs.existsSync(path.join(__dirname, '_fixture/package/_Pdfs'))).to.be.false;
     });
 
-    it('Should generate an app zip if app flag preset on branch', () => {
-        execSync('grunt clean:zip package --mocha=package --branch=package', opts);
+    describe('package', () => {    
+        before(() => {
+            execSync('grunt clean:zip package --mocha=package --branch=package', opts);
+        });
 
-        expect(glob.sync(path.join(__dirname, '_fixture/package/_Zips/*_App.zip'))).to.be.an('array').that.is.not.empty;
+        const requested = packages.filter(
+            (d) => d.zips?.length || d.zips == null
+        );
+
+        for(let i = 0; i < requested.length; i++){
+            const { name } = requested[i];
+
+            it(`Should generate an ${name} zip if ${name} flag preset on branch`, () => {
+                expect(glob.sync(path.join(__dirname, `_fixture/package/_Zips/*_${capitalize(name)}.zip`))).to.be.an('array').that.is.not.empty;
+            });
+        }
     });
 
     after(() => {
