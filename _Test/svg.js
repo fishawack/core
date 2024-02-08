7b12dd7f3775aa52a10531ffd3431010b7b6c0b1
Mike Mellor <mike.mellor@f-grp.com>
2024-02-08 13:33:54 +0000

    test: added artboard svg tests

--- _Test/svg.js
+++ _Test/svg.js
@@ -10,7 +10,7 @@ const { opts } = require('./_helpers/globals.js');
 describe('svg', () => {
     describe('fontello-svg', () => {
         before(() => {
-            execSync('grunt fontello_svg --branch=master --mocha=bundle', opts);
+            execSync('grunt clean:build fontello_svg --branch=master --mocha=bundle', opts);
         });
         
         it('Should download icons from fontello.com as svgs', () => {
@@ -23,18 +23,28 @@ describe('svg', () => {
     });
 
     describe('svgfit', () => {
+        let svg;
+        
         before(() => {
             execSync('grunt clean:build fontello_svg svgfit --branch=master --mocha=bundle', opts);
         });
         
         it('Should fit svgs to bounds', () => {
-            expect(glob.sync(path.join(__dirname, '_fixture/bundle/.tmp/icons-fit/*.svg'))).to.be.an('array').that.is.not.empty;
+            try{
+                svg = fs.readFileSync(path.join(__dirname, '_fixture/bundle/.tmp/icons-fit/artboard.svg'), opts);
+            } catch(e){
+                expect(e.message).to.not.contain('ENOENT');
+            }
+        });
+
+        it('Should fit viewBox tight to bounds', () => {
+            expect(svg).contain('viewBox="50 50 100 100"');
         });
     });
     
     describe('svgmin', () => {
         before(() => {
-            execSync('grunt fontello_svg svgfit svgmin --branch=master --mocha=bundle', opts);
+            execSync('grunt clean:build fontello_svg svgfit svgmin --branch=master --mocha=bundle', opts);
         });
         
         it('Should minify & optimize svgs', () => {
@@ -74,6 +84,47 @@ describe('svg', () => {
                 expect(svg).to.not.contain(' fill="#00f"');
                 expect(svg).to.not.contain(' stroke="#00f"');
             });
+
+            describe('artboard', () => {
+                it('Should generate svgs with --artboard suffix', () => {
+                    try{
+                        svg = fs.readFileSync(path.join(__dirname, '_fixture/bundle/.tmp/icons-min/artboard--artboard.svg'), opts);
+                    } catch(e){
+                        expect(e.message).to.not.contain('ENOENT');
+                    }
+                });
+
+                it('Should not remove viewBox', () => {
+                    expect(svg).contain('viewBox=');
+                });
+
+                it('Should leave viewBox unmodified', () => {
+                    expect(svg).contain('viewBox="0 0 200 200"');
+                });
+    
+                it('Should remove width & height', () => {
+                    expect(svg).to.not.contain(' width=');
+                    expect(svg).to.not.contain(' height=');
+                });
+    
+                it('Should prefix ids', () => {
+                    expect(svg).to.contain('id="id-');
+                });
+    
+                it('Should remove <style> blocks', () => {
+                    expect(svg).to.not.contain('<style>');
+                });
+    
+                it('Should remove useless fill & stroke', () => {
+                    expect(svg).to.not.contain(' stroke="red"');
+                    expect(svg).to.not.contain(' stroke-width="0"');
+                });
+    
+                it('Should remove fill & stroke', () => {
+                    expect(svg).to.not.contain(' fill="#00f"');
+                    expect(svg).to.not.contain(' stroke="#00f"');
+                });
+            });
         })
 
         describe('minimal', () => {
@@ -109,12 +160,53 @@ describe('svg', () => {
                 expect(svg).to.contain(' fill="#00f"');
                 expect(svg).to.contain(' stroke="#00f"');
             });
+
+            describe('artboard', () => {
+                it('Should generate svgs with --artboard suffix', () => {
+                    try{
+                        svg = fs.readFileSync(path.join(__dirname, '_fixture/bundle/.tmp/icons-min/--artboard--artboard.svg'), opts);
+                    } catch(e){
+                        expect(e.message).to.not.contain('ENOENT');
+                    }
+                });
+    
+                it('Should not remove viewBox', () => {
+                    expect(svg).contain('viewBox=');
+                });
+
+                it('Should leave viewBox unmodified', () => {
+                    expect(svg).contain('viewBox="0 0 200 200"');
+                });
+    
+                it('Should remove width & height', () => {
+                    expect(svg).to.not.contain(' width=');
+                    expect(svg).to.not.contain(' height=');
+                });
+    
+                it('Should prefix ids', () => {
+                    expect(svg).to.contain('id="id-');
+                });
+    
+                it('Should not remove <style> blocks', () => {
+                    expect(svg).to.contain('<style>');
+                });
+    
+                it('Should not remove useless fill & stroke', () => {
+                    expect(svg).to.contain(' stroke="red"');
+                    expect(svg).to.contain(' stroke-width="0"');
+                });
+    
+                it('Should not remove fill & stroke', () => {
+                    expect(svg).to.contain(' fill="#00f"');
+                    expect(svg).to.contain(' stroke="#00f"');
+                });
+            });
         })
     });
 
     describe('svgsprite', () => {
         before(() => {
-            execSync('grunt fontello_svg svgfit svgmin svg_sprite --branch=master --mocha=bundle', opts);
+            execSync('grunt clean:build fontello_svg svgfit svgmin svg_sprite --branch=master --mocha=bundle', opts);
         });
         
         it('Should generate svgSprite.svg', () => {
