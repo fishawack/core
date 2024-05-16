1fa1d53c3fe957a0822912e01aaf95a551cec162
Jeremy Viner <jeremyv@2ecreative.com>
2024-05-16 11:02:36 -0500

    feat: combine json config files into single file

--- _Tasks/handover.js
+++ _Tasks/handover.js
@@ -1,3 +1,5 @@
+const { deployBranch } = require('./helpers/include');
+
 module.exports = (grunt) => {
     grunt.registerTask('package:handover', ['clean:handover', 'copy:handover', 'handover']);
 
@@ -9,21 +11,26 @@ module.exports = (grunt) => {
 
         fs.writeFileSync(`${location}/.gitignore`, fs.readFileSync(`${location}/.gitignore`, 'utf8').replace('\n_Build/content', ''));
 
+        var baseConfig = {};
+        
         glob.sync(`${location}/${config.src}/config/**/*.json`)
             .forEach(d => {
-                var config = fs.readJSONSync(d);
-                [
-                    'deploy',
-                    'content',
-                    'phonegap',
-                    'veeva',
-                    'cegedim',
-                    'vablet',
-                    'email'
-                ].forEach(d => delete config.attributes[d]);
-
-                fs.writeFileSync(d, JSON.stringify(config, null, 4));
+                _.mergeWith(baseConfig,fs.readJSONSync(d));
             });
+        [
+            'deploy',
+            'content',
+            'phonegap',
+            'veeva',
+            'cegedim',
+            'vablet',
+            'email',
+            'targets',
+            'env'
+        ].forEach(d => delete baseConfig.attributes[d]);
+
+        fs.writeFileSync(`${location}/fw.json`,JSON.stringify(baseConfig,null,4));
+        fs.rmSync(`${location}/${config.src}/config/`, { recursive: true, force: true });
 
         let pkg = fs.readJSONSync(`${location}/package.json`);
         [
